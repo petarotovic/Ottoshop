ec4c50494061aac83656393a1470328e5197680a
Petar Otovic <otovicpetar1998@gmail.com>
2022-07-06 10:51:05 +0200

    MAC 10:50 6/7

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { StartingPageComponent } from './StartingPage/starting-page/starting-page.component';
-import { StyleDirective } from './Directives/style.directive';
 import { HeaderBannerMessageComponent } from './header/header-banner-message/header-banner-message.component';
 import { MainHeaderDivComponent } from './header/main-header-div/main-header-div.component';
 import { SizeDirective } from './Directives/size.directive';
@@ -15,7 +14,6 @@ import { SizeDirective } from './Directives/size.directive';
     AppComponent,
     HeaderComponent,
     StartingPageComponent,
-    StyleDirective,
     HeaderBannerMessageComponent,
     MainHeaderDivComponent,
     SizeDirective,

4bd9d6caed96eaa290c8490fe9fa0bdf963089fb
PeraSniper <petarotovic130@gmail.com>
2022-07-07 20:06:46 +0200

    Start Over

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,22 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { StartingPageComponent } from './StartingPage/starting-page/starting-page.component';
-import { HeaderBannerMessageComponent } from './header/header-banner-message/header-banner-message.component';
-import { MainHeaderDivComponent } from './header/main-header-div/main-header-div.component';
 import { SizeDirective } from './Directives/size.directive';
-import { DivImageGeneratorComponent } from './div-image-generator/div-image-generator.component';
-import { MenuLinkGeneratorComponent } from './menu-link-generator/menu-link-generator.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     StartingPageComponent,
-    HeaderBannerMessageComponent,
-    MainHeaderDivComponent,
-    SizeDirective,
-    DivImageGeneratorComponent,
-    MenuLinkGeneratorComponent,
+    SizeDirective
   ],
   imports: [
     BrowserModule,
